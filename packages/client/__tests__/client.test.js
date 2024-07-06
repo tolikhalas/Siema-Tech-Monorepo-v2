9fa7e11f0f0d93716000ed44922834f2504b60eb
Anatolii Halas <tolikgalas@gmail.com>
2024-07-06 19:01:21 +0300

    Config/ Added Next.js to client package

--- packages/client/__tests__/client.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const client = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(client(), 'Hello from client');
-console.info('client tests passed');
--- packages/client/lib/client.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = client;
-
-function client() {
-  return 'Hello from client';
-}
